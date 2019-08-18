6a6078cf5047ac0f97d6441bec70ab2857649d78
ChandraReddy74 <chandrasekhar.scm07@gmail.com>
2019-08-18 23:15:17 +0530

    third java file committing

---
+++ devops/src/main/java/project/App2.java
@@ -0,0 +1,13 @@
+package project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App2 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! pls continue the practice" );
+    }
+}
