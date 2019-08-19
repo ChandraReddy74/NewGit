7f5d721d734bc056abdfdc6de97aef8d01f513ee
ChandraReddy74 <chandrasekhar.scm07@gmail.com>
2019-08-19 09:57:14 +0530

    committing

---
+++ devops/src/main/java/project/App3.java
@@ -0,0 +1,13 @@
+package project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App3 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! pls continue the practice keep rocjking\" );
+    }
+}
